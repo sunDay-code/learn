6782669f25f12b3104f4eaa21527d9f50af6a08b
sunDay-code <changkaiyuan0916@gmail.com>
2021-01-08 16:04:57 -0800

    computerVision

---
+++ ComputerVision/.ipynb_checkpoints/OpenCV_drawWithMouse2-checkpoint.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+
+# VARIABLES
+
+#True while mouse button down, False while mouse button up
+drawing = False
+ix,iy=-1,-1
+
+# FUNCITONS
+def draw_rectangle(event,x,y,flags,params):
+    global ix,iy,drawing #able to change them in the global space
+    
+    if event == cv2.EVENT_LBUTTONDOWN:
+        
+        drawing = True
+        ix,iy = x,y
+    
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+            cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
+    
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
+
+# SHOWING IMAGES
+
+img = np.zeros(shape=(512,512,3))
+
+#connect functions to the window
+cv2.namedWindow(winname='my_drawing')
+cv2.setMouseCallback('my_drawing',draw_rectangle )
+
+while True:
+    
+    cv2.imshow('my_drawing',img)
+    
+    #check Esc key
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
