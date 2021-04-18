e5e56d067dfcc8f729f1311c920d926caffb8e8e
sunDay-code <changkaiyuan0916@gmail.com>
2021-04-17 21:05:12 -0700

    update

---
+++ design_pattern/singleton.py
@@ -0,0 +1,30 @@
+# Singleton
+# A "singleton" object: the lone object returned by
+# its class every time the class is called
+#
+# Reference: https://python-patterns.guide/gang-of-four/singleton/
+#
+
+class Logger(object):
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            print('Creating the object')
+            cls._instance = super(Logger, cls).__new__(cls)
+            # Put any initialization here.
+        return cls._instance
+
+
+def _main():
+    log1 = Logger()
+    print(log1)
+
+    log2 = Logger()
+    print(log2)
+
+    print("Are they the same object? ", log1 is log2)
+
+
+if __name__ == "__main__":
+    _main()
